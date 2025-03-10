f9ff4b285efbe21f7701af013479ab28432c797d
SvyatoslavJSfan <slava230880@mail.ru>
2025-03-10 20:38:24 +0300

    checks fixed

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -7,7 +7,8 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
+
+    const plugins = [
         new HTMLWebpackPlugin({
             template: paths.html
         }),
@@ -21,9 +22,14 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
             __IS_DEV__: JSON.stringify(isDev)
         }),
 
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false
-        })
     ]
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin())
+        plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))
+    }
+
+
+
+    return plugins
 }
