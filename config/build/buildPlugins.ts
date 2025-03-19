d51ca631694100e87c6fbd1c1949e43d2a25dd7e
SvyatoslavJSfan <slava230880@mail.ru>
2025-03-19 19:15:44 +0300

    lazy loading, reducer manager, dynamic module loader component added

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -24,11 +24,11 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
 
     ]
 
-    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))
-
+    
     if (isDev) {
         plugins.push(new webpack.HotModuleReplacementPlugin())
-        
+        plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))
+
     }
 
 
