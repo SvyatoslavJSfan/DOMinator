1b9119e98a119b26e4ee4558ed2b2d744b7e4b64
SvyatoslavJSfan <slava230880@mail.ru>
2025-09-04 15:51:24 +0300

    fixing PL 2

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { StoryFn } from '@storybook/react'
 import { Theme } from '@/shared/const/theme';
+import { ThemeProvider } from '../../../../app/providers/ThemeProvider';
+
 // eslint-disable-next-line path-fixer-tralnotipsus/layer-imports
-import { ThemeProvider } from '@/app/providers/ThemeProvider';
+
 
 
 
