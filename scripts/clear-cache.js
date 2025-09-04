e265a1b7600247faa7181ce108f501c19b50fad7
SvyatoslavJSfan <slava230880@mail.ru>
2025-09-04 15:18:36 +0300

    clear-cache fix

--- scripts/clear-cache.js
+++ scripts/clear-cache.js
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path')
+const path = require('path');
+const fs = require('fs');
 
-const fs = require('fs')
+const dirToRemove = path.resolve(__dirname, '..', 'node_modules', '.cache');
+console.log('Trying to remove cache:', dirToRemove);
 
-const dirToRemove = path.resolve(__dirname, '..','node_modules', '.cache')
-console.log(dirToRemove);
-
-
-fs.rmSync(dirToRemove, { recursive: true });
-
-console.log('CLEAR CACHE');
+if (fs.existsSync(dirToRemove)) {
+    fs.rmSync(dirToRemove, { recursive: true, force: true });
+    console.log('CLEAR CACHE ✅');
+} else {
+    console.log('No cache directory found, skipping ❌');
+}
