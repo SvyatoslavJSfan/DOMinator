9e2a996c01a37f6dce5409112b3f8835a1cc8774
SvyatoslavJSfan <slava230880@mail.ru>
2025-09-04 17:22:33 +0300

    fixing PL 7

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -3,6 +3,8 @@ export type Mods = Record<string, boolean | string | undefined>
 
 
 export function classNames(cls: string, mods: Mods = {}, additional: Array<string | undefined> = []): string {
+    console.log('Hi from classNames!');
+    
     return [
         cls, 
         ...additional.filter(Boolean), 
