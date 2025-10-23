c9fc48143afab10fc396e7433ed8a443a245b9a6
SvyatoslavJSfan <slava230880@mail.ru>
2025-10-23 20:13:06 +0300

    fixing server

--- json-server/index.js
+++ json-server/index.js
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs');
 const jsonServer = require('json-server');
-const jwt = require('jsonwebtoken')
 const path = require('path');
 const https = require('https');
+const http = require('http');
 
 const options = {
     key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
@@ -14,19 +14,7 @@ const server = jsonServer.create();
 
 const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
 
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
-});
-
-
+server.use(jsonServer.defaults({}));
 server.use(jsonServer.bodyParser);
 
 
@@ -37,24 +25,10 @@ server.use(async (req, res, next) => {
     next();
 });
 
-
-// eslint-disable-next-line
-// server.use((req, res, next) => {
-//     if (!req.headers.authorization) {
-//         return res.status(403).json({ message: 'AUTH ERROR' });
-//     }
-
-//     next();
-// });
-
-server.use(jsonServer.defaults());
-
-
-server.post('/login', async (req, res) => {
+// Эндпоинт для логина
+server.post('/login', (req, res) => {
     try {
         const { username, password } = req.body;
-        
-        
         const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
         const { users = [] } = db;
 
@@ -73,11 +47,29 @@ server.post('/login', async (req, res) => {
     }
 });
 
+// проверяем, авторизован ли пользователь
+// eslint-disable-next-line
+server.use((req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(403).json({ message: 'AUTH ERROR' });
+    }
+
+    next();
+});
+
 server.use(router);
 
+// запуск сервера
 const PORT = 8443;
-const httpsServer = https.createServer(options, server);
+const HTTP_PORT = 8000;
 
-httpsServer.listen(PORT, '127.0.0.1', () => {
-    console.log(`server is running on 127.0.0.1${PORT} port`);
+const httpsServer = https.createServer(options, server);
+const httpServer = http.createServer(server);
+
+httpsServer.listen(PORT, () => {
+    console.log(`server is running on ${PORT} port`);
+});
+
+httpServer.listen(HTTP_PORT, () => {
+    console.log(`server is running on ${HTTP_PORT} port`);
 });
