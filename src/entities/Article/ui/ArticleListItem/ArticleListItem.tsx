d26e86595a617e3e89321503054c41980a63a889
SvyatoslavJSfan <slava230880@mail.ru>
2025-11-12 21:52:21 +0300

    outline btn, skeleton, list view article

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -1,24 +1,13 @@
 /* eslint-disable i18next/no-literal-string */
 import React, { HTMLAttributeAnchorTarget, memo } from 'react';
-import { useTranslation } from 'react-i18next';
-import EyeIcon from '@/shared/assets/icons/eye-20-20.svg';
-import { classNames } from '@/shared/lib/classNames/classNames';
-import { AppLink } from '@/shared/ui/deprecated/AppLink';
-import { Avatar } from '@/shared/ui/deprecated/Avatar';
-import { Button, ThemeButton } from '@/shared/ui/deprecated/Button';
-import { Card } from '@/shared/ui/deprecated/Card';
-import { Icon } from '@/shared/ui/deprecated/Icon';
-import { Text } from '@/shared/ui/deprecated/Text';
-import { Article, ArticleTextBlock } from '../../model/types/article';
-import { ArticleBlockType, ArticleView } from '../../model/consts/articleConsts';
-import { ArticleTextBlockComponent } from '../ArticleTextBlockComponent/ArticleTextBlockComponent';
-import cls from './ArticleListItem.module.scss';
-import { getRouteArticleDetails } from '@/shared/const/router';
-import { AppImage } from '@/shared/ui/redesigned/AppImage';
-import { Skeleton } from '@/shared/ui/deprecated/Skeleton';
+import { Article } from '../../model/types/article';
+import { ArticleView } from '../../model/consts/articleConsts';
+import { ArticleListItemDeprecated } from './ArticleListItemDeprecated/ArticleListItemDeprecated';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { ArticleListItemRedesigned } from './ArticleListItemRedesigned/ArticleListItemRedesigned';
 
 
-interface ArticleListItemProps {
+export interface ArticleListItemProps {
     className?: string
     article: Article
     view: ArticleView
@@ -26,82 +15,12 @@ interface ArticleListItemProps {
 }
 
 
-export const ArticleListItem = memo(({ className, article, view, target }: ArticleListItemProps) => {
 
-    const { t } = useTranslation()
-
-
-    const types = <Text text={article.type.join(', ')} className={cls.types}/>
-    const views = (
-        <>
-            <Text text={String(article.views)} className={cls.views}/>
-            <Icon Svg={EyeIcon}/>
-        </>
-    )
-
-    if(view === ArticleView.BIG) {
-        const textBlock = article.blocks.find(block => block.type === ArticleBlockType.TEXT) as ArticleTextBlock
-
-        return (
-            <div 
-                data-testId="ArticleListItem"
-                className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}
-            >
-                <Card className={cls.card}>
-                    <div className={cls.header}>
-                        <Avatar size={30} src={article.user.avatar}/>
-                        <Text text={article.user.username} className={cls.username}/>
-                        <Text text={article.createdAt} className={cls.date}/>
-                    </div>
-                    <Text title={article.title} className={cls.title}/>
-                    {types}
-                    <AppImage 
-                        fallback={<Skeleton width="100%" height={250}/>}
-                        src={article.img} 
-                        className={cls.img} 
-                        alt={article.title}/>
-                    { textBlock && (
-                        <ArticleTextBlockComponent block={textBlock} className={cls.textBlock}/>
-                    )
-                    }
-                    <div className={cls.footer}>
-                        <AppLink 
-                            target={target}
-                            to={getRouteArticleDetails(article.id)}>
-                            <Button theme={ThemeButton.OUTLINE}>
-                                {t('Читать далее')}
-                            </Button>
-                        </AppLink>
-
-                        {views}
-                    </div>
-                </Card>
-            </div>
-        )
-    }
-
-    return (
-        <AppLink 
-            data-testId="ArticleListItem"
-            target={target}
-            to={getRouteArticleDetails(article.id)}
-            className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}>
-            <Card className={cls.card} >
-                <div className={cls.imageWrapper}>
-                    <AppImage 
-                        fallback={<Skeleton width={200} height={200}/>}
-                        src={article.img} 
-                        className={cls.img} 
-                        alt={article.title}/>
-                    <Text text={article.createdAt} className={cls.date}/>
-                </div>
-                <div className={cls.infoWrapper}>
-                    {types}
-                    {views}
-                </div>
-                <Text text={article.title} className={cls.title}/>
-            </Card>
-        </AppLink>
-    );
-})
+export const ArticleListItem = memo((props: ArticleListItemProps) => (
+    <ToggleFeatures
+        feature="isAppRedesigned"
+        on={<ArticleListItemRedesigned {...props} />}
+        off={<ArticleListItemDeprecated {...props} />}
+    />
+))
 
